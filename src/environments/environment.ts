d494b4c6ae003e2a9211473d85c7495b0d626f49
iamkanishka <kanishkabc123@gmail.com>
2022-03-12 23:56:52 +0530

    Added Firebase Configuation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'kanishkanaik-b3089',
+    appId: '1:13430891881:web:083a3858efd34a41f3c9f8',
+    storageBucket: 'kanishkanaik-b3089.appspot.com',
+    apiKey: 'AIzaSyCBIYX15eodP3OKiyYuAFCiM6tqrATk8no',
+    authDomain: 'kanishkanaik-b3089.firebaseapp.com',
+    messagingSenderId: '13430891881',
+    measurementId: 'G-JFPVZ4FB77',
+  },
   production: false
 };
 
